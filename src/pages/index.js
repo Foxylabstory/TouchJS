f0902a5a39583bdbb306976036fcf6589d1e1d32
Foxylab <rushan.shakirzianov@gmail.com>
2022-11-10 12:55:41 +0300

    feat: open and close modal window

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ import layout from '../vendor/keyboard/layout/russianLayout';
 // import layoutEn from '../vendor/keyboard/layout/englishLayout';
 
 const animateButton = document.querySelector('.header__animate');
+const modalWindow = document.querySelector('.popup');
+const closeModalWindow = modalWindow.querySelector('.popup__form-closer');
+
+// перенести установку значений в логику работы модального окна
 
 let setMinValue = -1000;
 let setMaxValue = 5000;
@@ -19,96 +23,107 @@ centerGauge.options.maxValue = setMaxValue;
 centerGauge.value = '10';
 const arr = [];
 for (let i = centerGauge.options.minValue; i <= centerGauge.options.maxValue; i = i + (centerGauge.options.maxValue + Math.abs(centerGauge.options.minValue)) / 10) {
-    arr.push(i);
+  arr.push(i);
 }
 centerGauge.options.majorTicks = arr;
 centerGauge.options.highlights = [
-    { "from": setMinValue, "to": minZone, "color": "rgba(255,0,0,.65)" },
-    { "from": maxZone, "to": setMaxValue, "color": "rgba(255,0,0,.75)" }
+  { "from": setMinValue, "to": minZone, "color": "rgba(255,0,0,.65)" },
+  { "from": maxZone, "to": setMaxValue, "color": "rgba(255,0,0,.75)" }
 ];
 centerGauge.draw();
 
 rightBottomGauge.options.maxValue = '440'
 rightBottomGauge.value = '100';
 rightBottomGauge.options.majorTicks = Array.from({ length: 11 }, function (value, index) {
-    // value будет undefined
-    return Math.round(index * (rightBottomGauge.options.maxValue / 10))
+  // value будет undefined
+  return Math.round(index * (rightBottomGauge.options.maxValue / 10))
 })
 rightBottomGauge.draw();
 
 console.log(centerGauge.options);
 linearGauge1.draw();
 
-
+// запуск генерации рандомных значений для индикаторов
 let timers = [];
 
 animateButton.addEventListener("click", function () {
-    document.gauges.forEach(function (gauge) {
-        timers.push(setInterval(function () {
-            gauge.value = Math.random() *
-                (gauge.options.maxValue - gauge.options.minValue) +
-                gauge.options.minValue;
-        }, gauge.animation.duration + 500));
-    });
+  document.gauges.forEach(function (gauge) {
+    timers.push(setInterval(function () {
+      gauge.value = Math.random() *
+        (gauge.options.maxValue - gauge.options.minValue) +
+        gauge.options.minValue;
+    }, gauge.animation.duration + 500));
+  });
 });
 
 // логика клавиатуры
 let keyboard = new Keyboard({
-    onChange: input => onChange(input),
-    onKeyPress: button => onKeyPress(button),
-    ...layout,
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button),
+  ...layout,
 });
 
 /**
  * Update simple-keyboard when input is changed directly
  */
 document.querySelector(".input").addEventListener("input", event => {
-    keyboard.setInput(event.target.value);
+  keyboard.setInput(event.target.value);
 });
 
 console.log(keyboard);
 
 function onChange(input) {
-    document.querySelector(".input").value = input;
-    console.log("Input changed", input);
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
 }
 
 function onKeyPress(button) {
-    console.log("Button pressed", button);
-    if (button === "{shift}" || button === "{lock}") handleShift();
-    if (button === "{En}" || button === "{Ru}") handleLang();
+  console.log("Button pressed", button);
+  if (button === "{shift}" || button === "{lock}") handleShift();
+  if (button === "{En}" || button === "{Ru}") handleLang();
 }
 
 function handleLang() {
-    let currentLayout = keyboard.options.layoutName;
-    let langToggle = '';
-    if (currentLayout === "default") {
-        langToggle = "defaultEn";
-    } else if (currentLayout === "shift") {
-        langToggle = "shiftEn";
-    } else if (currentLayout === "defaultEn") {
-        langToggle = "default";
-    } else if (currentLayout === "shiftEn") {
-        langToggle = "shift";
-    };
-    keyboard.setOptions({
-        layoutName: langToggle,
-    });
+  let currentLayout = keyboard.options.layoutName;
+  let langToggle = '';
+  if (currentLayout === "default") {
+    langToggle = "defaultEn";
+  } else if (currentLayout === "shift") {
+    langToggle = "shiftEn";
+  } else if (currentLayout === "defaultEn") {
+    langToggle = "default";
+  } else if (currentLayout === "shiftEn") {
+    langToggle = "shift";
+  };
+  keyboard.setOptions({
+    layoutName: langToggle,
+  });
 }
 
 function handleShift() {
-    let currentLayout = keyboard.options.layoutName;
-    let shiftToggle = '';
-    if (currentLayout === "default") {
-        shiftToggle = "shift";
-    } else if ( currentLayout === "shift" ) {
-        shiftToggle = "default";
-    } else if ( currentLayout === "defaultEn" ) {
-        shiftToggle = "shiftEn";
-    } else if ( currentLayout === "shiftEn" ) {
-        shiftToggle = "defaultEn";
-    };
-    keyboard.setOptions({
-        layoutName: shiftToggle,
-    });
+  let currentLayout = keyboard.options.layoutName;
+  let shiftToggle = '';
+  if (currentLayout === "default") {
+    shiftToggle = "shift";
+  } else if (currentLayout === "shift") {
+    shiftToggle = "default";
+  } else if (currentLayout === "defaultEn") {
+    shiftToggle = "shiftEn";
+  } else if (currentLayout === "shiftEn") {
+    shiftToggle = "defaultEn";
+  };
+  keyboard.setOptions({
+    layoutName: shiftToggle,
+  });
 }
+
+// открытие попапа
+document.addEventListener('click', function (event) {
+  console.log(event.target.id);
+  modalWindow.classList.add('popup_opened');
+}, true);
+
+// закрытие попапа
+closeModalWindow.addEventListener('click', function (event) {
+  modalWindow.classList.remove('popup_opened');
+});
